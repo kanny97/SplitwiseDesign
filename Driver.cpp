e8770cfb91c46125be076008ac5e07747b1ac272
Kanakdeep Chakraborty <33214024+kanny97@users.noreply.github.com>
2021-11-27 22:14:03 +0530

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -29,10 +29,10 @@ int main()
 {
     //Add initial users
     ExpenseManager expenseManager;
-    expenseManager.addUser(User("u1", "User1", "gaurav@workat.tech", "9876543210"));
-    expenseManager.addUser(User("u2", "User2", "sagar@workat.tech", "9876543210"));
-    expenseManager.addUser(User("u3", "User3", "hi@workat.tech", "9876543210"));
-    expenseManager.addUser(User("u4", "User4", "mock-interviews@workat.tech", "9876543210"));
+    expenseManager.addUser(User("u1", "User1", "abc@gmail.com", "1"));//Dummy values provided
+    expenseManager.addUser(User("u2", "User2", "def@gmail.com", "2"));
+    expenseManager.addUser(User("u3", "User3", "ghi@gmail.com", "3"));
+    expenseManager.addUser(User("u4", "User4", "xyz@gmail.com", "4"));
     while(true)
     {
         string inputLine;
